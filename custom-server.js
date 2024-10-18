d26461ce7ad41ccae8b17049caf27c410fc0c472
Aditya Dave <dave.aditya2017@gmail.com>
2024-10-18 14:41:27 -0500

    chunks

--- custom-server.js
+++
@@ -1,8 +0,0 @@
-const { createServer } = require('http');
-
-createServer((req, res) => {
-    if (req.url.endsWith('.js')) {
-        res.setHeader('Content-Type', 'text/javascript');
-    }
-    // ... rest of your server logic
-}).listen(8080);
