2f918bb6ba5f251651662921178ba4bceb430257
Aditya Dave <dave.aditya2017@gmail.com>
2024-10-24 01:21:14 -0500

    reference change

--- script.js
+++ script.js
@@ -146,8 +146,8 @@
 
 //version 3
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
